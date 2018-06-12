93ba1ed4f06edc685bba3b2caf58364786814752
Stallmanifold <stallmanifold@gmail.com>
2018-06-12 17:53:01 -0500

    Add Shader code and geometry assets.

--- demos/07_ray_picking/src/main.rs
+++ demos/07_ray_picking/src/main.rs
@@ -1,3 +1,11 @@
+const MESH_FILE: &str = "src/sphere.obj";
+const VERTEX_SHADER_FILE: &str = "src/test.vert.glsl";
+const FRAGMENT_SHADER_FILE: &str = "src/test.frag.glsl";
+const NUM_SPHERES: usize = 4;
+
+static mut PREVIOUS_SECONDS: f64 = 0.0;
+
+
 fn main() {
     println!("Hello, world!");
 }
