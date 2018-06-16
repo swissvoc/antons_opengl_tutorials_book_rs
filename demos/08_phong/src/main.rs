3a19ed6248a9d0195991fb6a8e812a37d660cf3c
Stallmanifold <stallmanifold@gmail.com>
2018-06-16 04:58:58 -0500

    Remove manually set perspective matrix code in favor of math library function.

--- demos/08_phong/src/main.rs
+++ demos/08_phong/src/main.rs
@@ -93,17 +93,7 @@ fn main() {
     let fov = 67.0 * math::ONE_DEG_IN_RAD; // convert 67 degrees to radians
     let aspect = unsafe { G_GL_WIDTH as f32 / G_GL_HEIGHT as f32 }; // aspect ratio
     // matrix components
-    let range = f32::tan(fov * 0.5) * near;
-    let Sx = (2.0 * near) / (range * aspect + range * aspect);
-    let Sy = near / range;
-    let Sz = -(far + near) / (far - near);
-    let Pz = -(2.0 * far * near) / (far - near);
-    let proj_mat = math::mat4(
-        Sx,   0.0,  0.0,  0.0, 
-        0.0,  Sy,   0.0,  0.0,
-        0.0,  0.0,  Sz,  -1.0, 
-        0.0,  0.0,  Pz,   0.0
-    );
+    let proj_mat = Mat4::perspective(fov, aspect, near, far);
 
     /* create VIEW MATRIX */
     let cam_pos = [0.0, 0.0, 2.0];   // don't start at zero, or we will be too close
@@ -116,48 +106,48 @@ fn main() {
     let mut model_mat = Mat4::identity();
 
     unsafe {
-    gl::UseProgram(shader_programme);
-    let view_mat_location = gl::GetUniformLocation(shader_programme, "view_mat".as_ptr() as *const i8);
-    assert!(view_mat_location != -1);
-    gl::UniformMatrix4fv(view_mat_location, 1, gl::FALSE, view_mat.as_ptr());
-    let proj_mat_location = gl::GetUniformLocation(shader_programme, "projection_mat".as_ptr() as *const i8);
-    assert!(proj_mat_location != -1);
-    gl::UniformMatrix4fv(proj_mat_location, 1, gl::FALSE, proj_mat.as_ptr());
-    let model_mat_location = gl::GetUniformLocation( shader_programme, "model_mat".as_ptr() as *const i8);
-    assert!(model_mat_location != -1);
-    gl::UniformMatrix4fv(model_mat_location, 1, gl::FALSE, model_mat.as_ptr());
-
-    gl::Enable(gl::CULL_FACE); // cull face
-    gl::CullFace(gl::BACK);    // cull back face
-    gl::FrontFace(gl::CW);     // GL_CCW for counter clock-wise
-
-    while !g_window.should_close() {
-        _update_fps_counter(&mut glfw, &mut g_window);
-        let current_seconds = glfw.get_time();
-
-        // wipe the drawing surface clear
-        gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
-        gl::Viewport(0, 0, G_GL_WIDTH as i32, G_GL_HEIGHT as i32);
-
         gl::UseProgram(shader_programme);
-
-        model_mat.m[12] = f32::sin(current_seconds as f32);
+        let view_mat_location = gl::GetUniformLocation(shader_programme, "view_mat".as_ptr() as *const i8);
+        assert!(view_mat_location != -1);
+        gl::UniformMatrix4fv(view_mat_location, 1, gl::FALSE, view_mat.as_ptr());
+        let proj_mat_location = gl::GetUniformLocation(shader_programme, "projection_mat".as_ptr() as *const i8);
+        assert!(proj_mat_location != -1);
+        gl::UniformMatrix4fv(proj_mat_location, 1, gl::FALSE, proj_mat.as_ptr());
+        let model_mat_location = gl::GetUniformLocation( shader_programme, "model_mat".as_ptr() as *const i8);
+        assert!(model_mat_location != -1);
         gl::UniformMatrix4fv(model_mat_location, 1, gl::FALSE, model_mat.as_ptr());
 
-        gl::BindVertexArray(vao);
-        // draw points 0-3 from the currently bound VAO with current in-use shader
-        gl::DrawArrays(gl::TRIANGLES, 0, 3);
-        // update other events like input handling
-        glfw.poll_events();
-        match g_window.get_key(Key::Escape) {
-            Action::Press | Action::Repeat => {
-                g_window.set_should_close(true);
-            }
-            _ => {}
-        }
+        gl::Enable(gl::CULL_FACE); // cull face
+        gl::CullFace(gl::BACK);    // cull back face
+        gl::FrontFace(gl::CW);     // GL_CCW for counter clock-wise
 
-        // put the stuff we've been drawing onto the display
-        g_window.swap_buffers();
-    }
+        while !g_window.should_close() {
+            _update_fps_counter(&mut glfw, &mut g_window);
+            let current_seconds = glfw.get_time();
+
+            // wipe the drawing surface clear
+            gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+            gl::Viewport(0, 0, G_GL_WIDTH as i32, G_GL_HEIGHT as i32);
+
+            gl::UseProgram(shader_programme);
+
+            model_mat.m[12] = f32::sin(current_seconds as f32);
+            gl::UniformMatrix4fv(model_mat_location, 1, gl::FALSE, model_mat.as_ptr());
+
+            gl::BindVertexArray(vao);
+            // draw points 0-3 from the currently bound VAO with current in-use shader
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+            // update other events like input handling
+            glfw.poll_events();
+            match g_window.get_key(Key::Escape) {
+                Action::Press | Action::Repeat => {
+                    g_window.set_should_close(true);
+                }
+                _ => {}
+            }
+
+            // put the stuff we've been drawing onto the display
+            g_window.swap_buffers();
+        }
     }
 }
